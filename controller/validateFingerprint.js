edde2062a306c83cb74f575eb40b8a06ec340a1d
mhkasif <haseebasif97@gmail.com>
2021-01-21 14:36:45 +0500

    removed unused code from file

--- controller/validateFingerprint.js
+++ controller/validateFingerprint.js
@@ -9,31 +9,23 @@ const validateFingerprint = async (req, res) => {
   //TODO:destruct the value indexOfFinger and voter_id and send to afis api
   try {
     const response = await Voter.findOne({ voter_id });
-    // console.log(response);
     if (response) {
-      // console.log(req.file)
       const data = new Form();
       data.append("link", response[index]);
-      // data.append("file" ,req.file.buffer,{filename: `${req.file.fieldname}.${req.file.mimetype.split('/')[0]}`, contentType: req.file.mimetype});
+
       data.append("file", req.file.buffer, {
         filename: `${req.file.fieldname}.${req.file.mimetype.split("/")[1]}`,
         contentType: req.file.mimetype,
       });
-      // // data.append({url:response[index]})
+
       const formHeader = data.getHeaders();
-      // const r = await axios({
-      //   method: "POST",
-      //   // headers: { "Content-Type": "multipart/form-data" },
-      //   // url: "https://vote-chain95.herokuapp.com/test",
-      //   url: "http://localhost:8800/test",
-      //   data
-      // });
-      // console.log(r)
-      await axios.post("http://localhost:8800/test", data, {
+
+     const resp =await axios.post("http://localhost:8800/test", data, {
         headers: { ...formHeader },
       });
+      console.log(resp)
       res.json({ voter: response });
-    } else res.json({ voter: "does not exist" });
+    } else res.json({ voter: "voter does not exist" });
   } catch (error) {
     console.log(error, "xxx");
     res.json({ error });
